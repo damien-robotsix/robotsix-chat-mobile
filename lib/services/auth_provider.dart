073fbd8bfad3ca29d71de875d971230730cba3cf
robotsix-mill <mill@robotsix.local>
2026-08-23 16:22:21 +0000

    ci: auto-fix remove redundant meta/meta.dart import (foundation.dart already exports it)

--- lib/services/auth_provider.dart
+++ lib/services/auth_provider.dart
@@ -4,7 +4,6 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
-import 'package:meta/meta.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import 'api_service.dart';
